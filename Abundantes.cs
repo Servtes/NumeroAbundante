1e8ac113e9ef168c07c489e13d1d2830695bd51e
Jose Sosa <jose_sosa@ymail.com>
2014-10-19 21:55:21 -0600

    Algoritmo
    Codigo Fuente

---
+++ Abundantes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+
+       static void determinarAbundante(int n) 
+        {
+            int a = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a = a + i;
+                }
+
+            }
+
+            if (a > n)
+            {
+                Console.WriteLine("El número "+n+ " es abundante ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros Impares [Abundantes] del 0 al 5000 ");
+            for (int i = 0; i < 5000; i++) 
+            {
+                if (i % 2 != 0)
+                {
+                    determinarAbundante(i);                
+                }            
+            }
+           Console.ReadKey();
+        }    
+    }
+}
